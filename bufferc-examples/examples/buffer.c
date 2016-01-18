0848d29f571534a56a032711df0083d58b633915
Tim van Rossum <trvanrossum@gmail.com>
2016-01-18 16:16:08 +0100

    Started C runtime lib

--- bufferc-examples/examples/buffer.c
+++ bufferc-examples/examples/buffer.c
@@ -5,11 +5,39 @@
 // This is the run-time library implementing the primitive operations for
 // the BufferC buffer data type.
 
+/* STRUCT */
+typedef struct {
+	char* ptr;
+	int bufsize;
+	int refCount = 0;
+} buffer;
+
+// Allocation, copying and dereferencing.
+buffer *alloc_buf(int size) {
+	buffer *buf = (buffer *)malloc(sizeof(buffer));
+	buf->bufsize = size;
+	buf->ptr = (char *)malloc(buf->bufsize);
+	buf->refCount = buf->refCount + 1;
+	return buf;
+}
+
+buffer *copy(buffer *src) {
+	buffer *dst = alloc_buf(src->bufsize);
+	strncpy(dst->ptr, src->ptr, dst->bufsize);
+	dst->ptr[dst->bufsize-1] = '\0';
+	return dst;
+}
+
 /* PRINTING */
 
 /* FILE OPERATIONS */
 
 /* REFERENCE COUNTING */
-
+void deref_buf(buffer *buf){
+	buf->refCount = buf->refCount - 1;
+	if(refCount = 0){
+		free(buf);
+	}
+}
 
 
