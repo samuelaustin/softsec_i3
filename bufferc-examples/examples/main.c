95d9b6333f3f1a8c0943452b3176f117b5a507fb
Tim van Rossum <trvanrossum@gmail.com>
2016-01-30 15:37:13 +0100

    Added tests to the main method.

--- bufferc-examples/examples/main.c
+++ bufferc-examples/examples/main.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include "buffer.h"
 #include "memoryleak.c"
+#include "bounds.h"
+#include "test_printf.h"
+#include "test_types.h"
 
 int main( int argc, const char* argv[] )
 {
+    within_bounds_read();
+    within_bounds_assign();
+    printf("This should work.");
+    out_of_bounds_read();
+    out_of_bounds_assign();
+    printf("This should not work.");
 	leak();
+	printf("Should not give a buffer overflow.");
+	buffer *b = alloc_buf(10);
+	f(b);
+    printf(inc(5) == 6);
+    printf(integer_expressions());
+    printf(boolean_expressions());
+    printf(buffer_expressions());
+    printf(print_expressions());
+	return 0;
 }
--- bufferc-examples/examples/memoryleak.c
+++ bufferc-examples/examples/memoryleak.c
@@ -1,18 +1,19 @@
 
 #include <stdlib.h>
 #include "buffer.h"
+#include "buffer.c"
 #include "memoryleak.h"
-	
-	
+
+
 	void leak() {
 	buffer * b;
-	
-	for(int i = 0; (i <  1000000); i++) {
-			deref_buf(b); 
+	int i;
+	for(i = 0; (i <  1000000); i++) {
+			deref_buf(b);
 			b = alloc_buf((100));
 		}
-	
-	printf((alloc_buf_string(9-2, "success"))->ptr);
-	
+
+	printf("success");
+
 	}
-	
+
